812bd9c770ce331196582f81d0a9238b5fd44b00
Harish Dhanraj Sugandhi <harishsugandhi9890@gmail.com>
2026-07-23 19:08:27 +0530

    tally 0.1.7

--- Casks/tally.rb
+++ Casks/tally.rb
@@ -1,9 +1,9 @@
 cask "tally" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.1.6"
-  sha256 arm:   "c4d4cab70ec98d526aaad58719dd833d0566f259b973d9d72da0bc0255f1f4d5",
-         intel: "74adabded1ebf76bf40c5ab4c5c53fdf9167fb8e23ab4d829d6cb48d564dd3e6"
+  version "0.1.7"
+  sha256 arm:   "a811aca9a81f90a2fcc533c0f10aea5d2f179085309aea986c392799b17a18b9",
+         intel: "9f8cfeb9a21dcbcb6af49d7c9ed667d72067c63469d97dcc3ec05f96c9545dff"
 
   url "https://github.com/earth9890/tally/releases/download/v#{version}/Tally-#{version}-#{arch}.dmg"
   name "Tally"
